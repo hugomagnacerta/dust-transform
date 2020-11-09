f4fc4a428ef54581ba3f616dda3d024d2d0de69a
Itazabi RL <itazabirl@MacBook-Pro-de-Itazabi.local>
2020-11-08 22:14:56 -0600

    stable version

--- src/index.js
+++ src/index.js
@@ -6,11 +6,23 @@ const fs = require('fs');
 /* internal modules */
 const jsonData = require('./resources/json/data.json');
 
-const fileName = join(__dirname,'resources', 'templates', 'basic.dust');
+dust.helpers.toString = (chunk, context, bodies, { string }) => {
+  if ( !string ) return 'N/A';
+  //process string data
+  return string;
+}
+dust.helpers.toDate = (chunk, context, bodies, {date}) => {
+  if ( !date ) return 'N/A';
+
+  let dateWithFormat = new Date(date);
+
+  return dateWithFormat.toISOString();
+}
+
+const fileName = join(__dirname,'resources', 'templates', 'main.dust');
 
 const source = fs.readFileSync(fileName, 'utf8');
 const compiled = dust.compile(source, 'tmpl');
-
 
 dust.loadSource(compiled);
 dust.render('tmpl', jsonData, function(err, out) {
